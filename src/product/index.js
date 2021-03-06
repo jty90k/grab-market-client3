27b974fd60b80a98a1e900b23f35c7ad1c1cf17f
UniverseStark <jty90k@gmail.com>
2021-03-06 13:37:31 +0900

    상세페이지 css추가

--- src/product/index.js
+++ src/product/index.js
@@ -1,3 +1,4 @@
+import "./index.css";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import { useEffect, useState } from "react";
@@ -34,6 +35,7 @@ function ProductPage() {
       <div id="contents-box">
         <div id="name">{product.name}</div>
         <div id="price">{product.price}원</div>
+        <div id="createdAt">2021년 3월 6일</div>
         <div id="description">{product.description}</div>
       </div>
     </div>
@@ -41,11 +43,3 @@ function ProductPage() {
 }
 
 export default ProductPage;
-// Cannot read property 'imageUrl' of null이 뜨는 경우는 null에 있는 imageUrl을 읽을 수가 없는 경우이다. 왜냐하면
-// useState(null) = proudct(img src="image/icons/avatar.png")이다.
-// product가 초기에는 null값이다.
-// 우리가 예상하기에는 product가 네트워크 통신했을 때 product는 결과적으로 객체안에 imageUrl이 있었다. 그런데
-// ./product/index.js에서 axios는 비동기 통신이다. 그렇기 때문에 처음에 const [product, setProduct] 중 product는 null값이 들어간체로 네트워크 통신이 실행이 된다.
-// 그다음에 결과를 받기 전에 바로 rendering이 되면 return이 실행이 될 때 product.imageUrl에서 product의 초기값은 null 이기 때문에 null.imageUrl이기 때문에 오류가 생긴다.
-// product값에 정산값이 들어 가면 아무 문제가 없지만 그렇지 않은 경우 방어코드를 만들어야 한다.
-// if문을 작성한다.
